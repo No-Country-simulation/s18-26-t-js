538cd97a8918bbf4ba92951ca04cc113fd8109a2
ae95caba <ae95caba@gmail.com>
2024-10-15 20:45:01 -0300

    feat: :sparkles: add feature but without accent insesitive query

--- src/app/api/restaurant/route.js
+++ src/app/api/restaurant/route.js
@@ -1,20 +1,39 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
+import validator from 'validator';
 
 const prisma = new PrismaClient();
 
 // Handler GET
-export async function GET() {
+export async function GET(req) {
   try {
+    // Extract query parameters from the URL
+    const { searchParams } = new URL(req.url);
+    let name = searchParams.get('name');
+    let city = searchParams.get('city');
+
+    // Sanitize inputs
+    if (name) name = validator.escape(name);
+    if (city) city = validator.escape(city);
+
+    // Create a filter object based on provided parameters
+    const filters = {};
+    if (name) filters.name = { contains: name, mode: 'insensitive' }; // Case-insensitive search
+    if (city) filters.city = { contains: city, mode: 'insensitive' }; // Case-insensitive search
+
     const restaurants = await prisma.restaurant.findMany({
+      where: filters, // Apply filters if any,
       include: {
-        reviews: true
-      }
+        reviews: true,
+      },
     });
     return NextResponse.json(restaurants, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Error al obtener los restaurantes' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Error al obtener los restaurantes' },
+      { status: 500 },
+    );
   }
 }
 
@@ -22,7 +41,16 @@ export async function GET() {
 export async function POST(req) {
   try {
     const body = await req.json(); // Parsear el cuerpo de la solicitud
-    const { name, city, location, phone, averageRating, imageUrl, logoUrl, description} = body;
+    const {
+      name,
+      city,
+      location,
+      phone,
+      averageRating,
+      imageUrl,
+      logoUrl,
+      description,
+    } = body;
 
     const newRestaurant = await prisma.restaurant.create({
       data: {
@@ -39,6 +67,9 @@ export async function POST(req) {
     return NextResponse.json(newRestaurant, { status: 201 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Error al crear el restaurante' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Error al crear el restaurante' },
+      { status: 500 },
+    );
   }
 }
