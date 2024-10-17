103d310a99daac689106f1edebd3a0a565fc635e
LautaroLD <duranlautarogabriel@gmail.com>
2024-10-17 17:39:45 -0300

    update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@
 import { BiSearch } from 'react-icons/bi';
 import UserMenuHeader from '@/components/UserMenuHeader';
 import { SessionProvider } from 'next-auth/react';
+import Link from 'next/link';
 export default function Header() {
   return (
     <header className='grid grid-flow-col py-5 md:px-20 sm:px-10 px-2 bg-[#FFFFFA] shadow-lg gap-2'>
       <div>
-        <picture className='sm:text-5xl text-2xl'>Logo</picture>
+        <Link className='sm:text-5xl text-2xl' href={'/'}>
+          <picture>Logo</picture>
+        </Link>
       </div>
       <div className='flex justify-center'>
         <div className='border rounded-lg px-3 py-1 flex w-full max-w-sm items-center'>
