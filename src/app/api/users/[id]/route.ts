056cc7817076aaefa5d1e8d1359956a69014f8cc
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-10-25 18:48:33 -0300

    update comment

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/libs/db';
 
-
 // GET .../api/users/[id]
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
