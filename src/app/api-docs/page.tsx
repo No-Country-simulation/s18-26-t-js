11ac456ba970a41c4a6cb3db1bf9182284be376a
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-10-26 10:43:57 -0300

    update: api-docs

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -7,6 +7,7 @@ const ApiDocs: React.FC = () => {
       <div className="bg-gray-800 rounded-lg shadow-lg p-6 w-full max-w-3xl">
         <h1 className="text-3xl font-bold mb-4">Documentación de la API</h1>
 
+        {/* Sección de Autenticación */}
         <h2 className="text-2xl font-semibold mt-6">Autenticación</h2>
         <pre className="bg-gray-700 p-4 rounded">
           <code>
@@ -14,6 +15,7 @@ const ApiDocs: React.FC = () => {
           </code>
         </pre>
 
+        {/* Sección de Restaurantes */}
         <h2 className="text-2xl font-semibold mt-6">Restaurantes</h2>
         <pre className="bg-gray-700 p-4 rounded">
           <code>
@@ -27,14 +29,43 @@ const ApiDocs: React.FC = () => {
             <br />
             <span className="text-red-400">DELETE</span> /api/restaurant/[id] - Eliminar un restaurante por ID.
             <br />
-            <span className="text-blue-400">GET</span> /api/restaurant/cities - Obtener ciudades de restaurantes (sin duplicados).
-            <br />
-            <span className="text-blue-400">GET</span> /api/restaurant/most-reviewed - Obtener los restaurantes más reseñados.
-            <br />
-            <span className="text-blue-400">GET</span> /api/restaurant/top-rated - Obtener los restaurantes mejor valorados.
+            <span className="text-blue-400">GET</span> /api/restaurant/search - Buscar restaurante por nombre.
           </code>
         </pre>
 
+        {/* Sección de Categorías */}
+        <h2 className="text-2xl font-semibold mt-6">Categorías</h2>
+        <pre className="bg-gray-700 p-4 rounded">
+          <code>
+            <span className="text-blue-400">GET</span> /api/categories - Obtener todas las categorías.
+            <br />
+            <span className="text-green-400">POST</span> /api/categories - Crear una nueva categoría.
+            <br />
+            <span className="text-blue-400">GET</span> /api/categories/[id] - Obtener una categoría por ID.
+            <br />
+            <span className="text-yellow-400">PUT</span> /api/categories/[id] - Actualizar una categoría por ID.
+            <br />
+            <span className="text-red-400">DELETE</span> /api/categories/[id] - Eliminar una categoría por ID.
+          </code>
+        </pre>
+
+        {/* Sección de Ciudades */}
+        <h2 className="text-2xl font-semibold mt-6">Ciudades</h2>
+        <pre className="bg-gray-700 p-4 rounded">
+          <code>
+            <span className="text-blue-400">GET</span> /api/cities - Obtener todas las ciudades.
+            <br />
+            <span className="text-green-400">POST</span> /api/cities - Crear una nueva ciudad.
+            <br />
+            <span className="text-blue-400">GET</span> /api/cities/[id] - Obtener una ciudad por ID.
+            <br />
+            <span className="text-yellow-400">PUT</span> /api/cities/[id] - Actualizar una ciudad por ID.
+            <br />
+            <span className="text-red-400">DELETE</span> /api/cities/[id] - Eliminar una ciudad por ID.
+          </code>
+        </pre>
+
+        {/* Sección de Reseñas */}
         <h2 className="text-2xl font-semibold mt-6">Reseñas</h2>
         <pre className="bg-gray-700 p-4 rounded">
           <code>
@@ -54,14 +85,24 @@ const ApiDocs: React.FC = () => {
           </code>
         </pre>
 
-        <h2 className="text-2xl font-semibold mt-6">Usuario</h2>
+        {/* Sección de Usuario */}
+        <h2 className="text-2xl font-semibold mt-6">Usuarios</h2>
         <pre className="bg-gray-700 p-4 rounded">
           <code>
-            <span className="text-blue-400">GET</span> /api/user/profile - Obtener el perfil del usuario.
+            <span className="text-blue-400">GET</span> /api/users - Obtener todos los usuarios.
+            {/* <br />
+            <span className="text-green-400">POST</span> /api/users - Crear un nuevo usuario. */}
             <br />
-            <span className="text-yellow-400">PUT</span> /api/user/profile - Actualizar el perfil del usuario.
+            <span className="text-blue-400">GET</span> /api/users/[id] - Obtener un usuario por ID.
+            {/* <br />
+            <span className="text-yellow-400">PUT</span> /api/users/[id] - Actualizar un usuario por ID. */}
+            {/* <br />
+            <span className="text-red-400">DELETE</span> /api/users/[id] - Eliminar un usuario por ID. */}
+            <br />
+            <span className="text-yellow-400">PUT</span> /api/users/[id]/update - Actualizar información del perfil del usuario.
           </code>
         </pre>
+
       </div>
     </div>
   );
