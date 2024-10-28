b27ab97f86e157fb5ca5613176105db001668164
LautaroLD <duranlautarogabriel@gmail.com>
2024-10-28 13:51:39 -0300

    create register restaurant form

--- src/app/new-restaurant/page.tsx
+++ src/app/new-restaurant/page.tsx
@@ -1,173 +1,39 @@
-'use client';
-import React, { useState } from 'react';
-import { LuImagePlus } from 'react-icons/lu';
-
-export default function Page() {
-  const [logoSelected, setLogoSelected] = useState<string | ArrayBuffer>();
-  const [bannerSelected, setBannerSelected] = useState<string | ArrayBuffer>();
-  const [categoriesSelected, setCategoriesSelected] = useState<string[]>([]);
-  const restaurantCategories: string[] = [
-    'Italiana',
-    'Japonesa',
-    'Mexicana',
-    'China',
-    'India',
-    'Francesa',
-    'Española',
-    'Tailandesa',
-    'Griega',
-    'Americana',
-    'Cafetería',
-    'Panadería',
-    'Heladería',
-    'Food Truck',
-    'Bar de Tapas',
-    'Pizzería',
-    'Parrilla',
-    'Buffet',
-    'Comida Rápida',
-    'Vegetariano',
-  ];
-  const loadNewImage = (
-    newImage: File,
-  ): Promise<string | ArrayBuffer | null> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(newImage);
-
-      reader.onload = (e) => {
-        resolve(e.target?.result as string);
-      };
-
-      reader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
+import NewRestaurantForm from '@/components/NewRestaurantForm';
+import { getSession } from '@/libs/actions';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+export default async function Page() {
+  const session = await getSession();
+  console.log(session);
+  if (!session) {
+    redirect('/auth/login');
+  }
   return (
-    <main className='px-20 py-10 flex flex-col gap-6 bg-[#FBFBF4]'>
-      <h1 className='text-3xl text-[#FB6800]'>Agregar restaurante</h1>
-      <section className='grid grid-cols-2'>
-        <form className='flex flex-col gap-3'>
-          <input
-            type='text'
-            className='border rounded-lg py-1 px-3'
-            placeholder='Nombre'
-          />
-          <select
-            className='border rounded-lg py-1 px-3 w-fit'
-            name=''
-            id=''
-            defaultValue={''}
-          >
-            <option value='' disabled>
-              Ciudad
-            </option>
-            <option value='Buenos Aires'>Buenos Aires</option>
-            <option value='Córdoba'>Córdoba</option>
-            <option value='Rosario'>Rosario</option>
-            <option value='San Miguel de Tucumán'>San Miguel de Tucumán</option>
-          </select>
-          <input
-            type='text'
-            className='border rounded-lg py-1 px-3'
-            placeholder='Dirección'
-          />
-          <input
-            type='text'
-            className='border rounded-lg py-1 px-3'
-            placeholder='Teléfono'
-          />
-          <input
-            type='text'
-            className='border rounded-lg py-1 px-3'
-            placeholder='Descripción'
-          />
-          <label>
-            <p>Selecciona las categorías </p>
-            <ul className='flex flex-wrap gap-2'>
-              {restaurantCategories.map((category, index) => (
-                <li
-                  key={index}
-                  className={`flex hover:cursor-pointer border p-1 rounded-lg ${categoriesSelected.includes(category) && 'bg-white border-[#F5D03A]'}`}
-                  onClick={() => {
-                    if (categoriesSelected.includes(category)) {
-                      setCategoriesSelected((prev) => {
-                        const newList = prev.filter((i) => i !== category);
-                        return newList;
-                      });
-                    } else {
-                      setCategoriesSelected([...categoriesSelected, category]);
-                    }
-                  }}
-                >
-                  {category}
-                </li>
-              ))}
-            </ul>
-          </label>
-
-          <label className='w-1/4'>
-            <p>Agrega el logo de tu restaurante</p>
-            <div className='border rounded-xl relative aspect-video flex '>
-              {logoSelected ? (
-                <img
-                  src={logoSelected as string}
-                  alt=''
-                  className='object-fill'
-                />
-              ) : (
-                <>
-                  <div className='m-auto flex flex-col justify-center items-center'>
-                    <LuImagePlus size={30} />
-                    <p>Haz click para agregar fotos.</p>
-                  </div>
-                  <input
-                    type='file'
-                    className='absolute z-10 inset-0 opacity-0 cursor-pointer'
-                    onChange={async (e) => {
-                      if (e.target.files?.item(0)) {
-                        const file = e.target.files[0];
-                        const imageData = await loadNewImage(file);
-                        setLogoSelected(imageData as string);
-                      }
-                    }}
-                    placeholder=''
-                  />
-                </>
-              )}
-            </div>
-          </label>
-          <label className='w-1/3'>
-            <p>Agrega la portada de tu restaurante</p>
-            <div className='border rounded-xl relative aspect-video flex '>
-              {bannerSelected ? (
-                <img src={bannerSelected as string} alt='' />
-              ) : (
-                <>
-                  <div className='m-auto flex flex-col justify-center items-center'>
-                    <LuImagePlus size={30} />
-                    <p>Haz click para agregar fotos.</p>
-                  </div>
-                  <input
-                    type='file'
-                    className='absolute z-10 inset-0 opacity-0 cursor-pointer'
-                    onChange={async (e) => {
-                      if (e.target.files?.item(0)) {
-                        const file = e.target.files[0];
-                        const imageData = await loadNewImage(file);
-                        setBannerSelected(imageData as string);
-                      }
-                    }}
-                    placeholder=''
-                  />
-                </>
-              )}
-            </div>
-          </label>
-        </form>
-        <aside></aside>
+    <main className='px-20 py-7 grid grid-cols-2 gap-6 bg-[#FBFBF4]'>
+      <section className='flex flex-col gap-6'>
+        <h1 className='text-3xl text-[#FB6800]'>Agregar restaurante</h1>
+        <div>
+          <NewRestaurantForm userId={session.user.id} />
+        </div>
       </section>
+      <aside className='flex justify-end items-start gap-4'>
+        <picture className='w-1/3 relative aspect-square mt-24'>
+          <Image
+            src={'/img/new-restaurant-2.png'}
+            alt=''
+            fill
+            className='object-contain'
+          />
+        </picture>
+        <picture className='w-1/3 relative aspect-square'>
+          <Image
+            src={'/img/new-restaurant-1.png'}
+            alt=''
+            fill
+            className='object-contain'
+          />
+        </picture>
+      </aside>
     </main>
   );
 }
--- src/app/profileUser/page.jsx
+++ src/app/profileUser/page.jsx
@@ -4,8 +4,9 @@ import { getSession } from '@/libs/actions';
 import axios from 'axios';
 import { redirect } from 'next/navigation';
 import RestaurantFormTest from './RestaruantFormTest';
-import ActualizarPerfil from "@/components/ActualizarPerfil"
- 
+import ActualizarPerfil from '@/components/ActualizarPerfil';
+import NewRestaurantCard from '@/components/NewRestaurantCard';
+
 const getUserRestaurants = async (userId, owner) => {
   if (owner) {
     // traer los restaurantes registrados por este usuario
@@ -13,14 +14,16 @@ const getUserRestaurants = async (userId, owner) => {
     return res.data;
   } else {
     // traer los restaurantes reseñados y calificados por este usuario
-    const res = await axios(`${process.env.NEXTAUTH_URL}/api/reviews/usuario/${userId}`);
+    const res = await axios(
+      `${process.env.NEXTAUTH_URL}/api/reviews/usuario/${userId}`,
+    );
     return res.data;
   }
 };
 
 export default async function ProfileUser() {
-  console.log(process.env.NEXTAUTH_URL)
-  let Profiledate // datos de usuario 
+  console.log(process.env.NEXTAUTH_URL);
+  let Profiledate; // datos de usuario
   const session = await getSession();
 
   if (!session) {
@@ -28,10 +31,9 @@ export default async function ProfileUser() {
   }
 
   const { id, owner, username } = session.user;
- await axios.get(`${process.env.NEXTAUTH_URL}/api/users/${id}`)
-  .then( res => {
-     Profiledate = res.data
-  })
+  await axios.get(`${process.env.NEXTAUTH_URL}/api/users/${id}`).then((res) => {
+    Profiledate = res.data;
+  });
   const restaurantsList = await getUserRestaurants(id, owner);
 
   return (
@@ -60,7 +62,6 @@ export default async function ProfileUser() {
             <img
               src={'/img/user-2.png'}
               alt='imagen de usuario'
-             
               className='w-full h-full rounded-full object-cover object-center '
             />
           </div>
@@ -86,7 +87,7 @@ export default async function ProfileUser() {
               <h6 className='font-medium'>Nivel:</h6>
               <p>Experto</p>
             </section>
-            <ActualizarPerfil PerfilDatos={Profiledate} id={id}/>
+            <ActualizarPerfil PerfilDatos={Profiledate} id={id} />
           </div>
         </div>
 
@@ -95,7 +96,11 @@ export default async function ProfileUser() {
             className='flex flex-wrap gap-2 text-2xl font-semibold p-2 max-w-max
                  border-[#45424C7A] border-[1px] rounded-md mb-5'
           >
-            <h3>{Profiledate.name} {Profiledate.lastName}</h3>|<h3>Comensal frecuente</h3><ActualizarPerfil PerfilDatos={Profiledate}/>
+            <h3>
+              {Profiledate.name} {Profiledate.lastName}
+            </h3>
+            |<h3>Comensal frecuente</h3>
+            <ActualizarPerfil PerfilDatos={Profiledate} />
           </div>
 
           <h6 className='ml-2 text-xl my-1'>Biografía</h6>
@@ -105,38 +110,46 @@ export default async function ProfileUser() {
               considero una persona amante de la gastronomía. En mis tiempos
               libres me dedico a buscar y reseñar restaurantes/cafeterías.
             </p>
-            <ActualizarPerfil PerfilDatos={Profiledate}/>
+            <ActualizarPerfil PerfilDatos={Profiledate} />
           </section>
-          <h6 className='text-xl mb-3'>Mis reseñas actuales</h6>
+          <div></div>
+          {owner ? (
+            <h6 className='text-xl mb-3'>Gestión de restaurantes</h6>
+          ) : (
+            <h6 className='text-xl mb-3'>Mis reseñas actuales</h6>
+          )}
           <div className=' ctn-reseñas overflow-x-auto h-[211px]  m-auto relative'>
-            { restaurantsList.length > 0 ? 
-            <ul className=' flex gap-3  absolute top-0  '>
-            { restaurantsList.map((receñas) => (
-              <li
-                key={receñas.id}
-                className='w-[365px] border-[#45424C7A] border-[1px] rounded-md '
-              >
-                <img
-                  src={receñas.image}
-                  alt={receñas.name}
-                  width={'100%'}
-                  className='block object-center object-cover h-[111px] rounded-t-md'
-                />
-                <div className='relative  rounded-b-md'>
-                  <section className='flex gap-1 absolute right-2 top-1'>
-                    <Estrella color='yellow' />
-                    <p className='text-[#45424C7A] text-[18px]'>
-                      {receñas.averageRating}
-                    </p>
-                  </section>
-                  <section className='flex gap-4 font-medium px-5 py-4'>
-                    <p>{receñas.name}</p> | <p>{receñas.location}</p>
-                  </section>
-                </div>
-              </li>
-            ))}
-          </ul>
-          : <p>No tienes reseñas</p>}
+            {restaurantsList.length > 0 ? (
+              <ul className=' flex gap-3  absolute top-0  '>
+                {owner && <NewRestaurantCard />}
+                {restaurantsList.map((receñas) => (
+                  <li
+                    key={receñas.id}
+                    className='w-[365px] border-[#45424C7A] border-[1px] rounded-md '
+                  >
+                    <img
+                      src={receñas.image}
+                      alt={receñas.name}
+                      width={'100%'}
+                      className='block object-center object-cover h-[111px] rounded-t-md'
+                    />
+                    <div className='relative  rounded-b-md'>
+                      <section className='flex gap-1 absolute right-2 top-1'>
+                        <Estrella color='yellow' />
+                        <p className='text-[#45424C7A] text-[18px]'>
+                          {receñas.averageRating}
+                        </p>
+                      </section>
+                      <section className='flex gap-4 font-medium px-5 py-4'>
+                        <p>{receñas.name}</p> | <p>{receñas.location}</p>
+                      </section>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No tienes reseñas</p>
+            )}
           </div>
         </div>
       </div>
