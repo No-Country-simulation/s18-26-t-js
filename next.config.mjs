a25b1c54fe34dd25e484f62828f69dddc88d7d76
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-10-30 19:35:50 -0300

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos', 'dummyimage.com', 's3-alpha-sig.figma.com','res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
